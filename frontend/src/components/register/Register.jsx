a655f22c7d3cec78db245814e1a26586637efcc6
Ranjan1501 <ranjankumar1757@gmail.com>
2022-04-25 16:28:36 +0530

    removed extra things

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./register.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Home from "../homepage/Home";
+
 
 const Register = () => {
   const navigate = useNavigate();
